cd12d2567498fef3f05b2132b37fe188b5417287
Ivan Zubenko <vanya.zubenko@gmail.com>
2021-07-08 16:49:26 +0300

    update python version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     packages=find_packages(),
     install_requires=install_requires,
     setup_requires=setup_requires,
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     entry_points={
         "console_scripts": [
             "platform-container-runtime=platform_container_runtime.api:main"
