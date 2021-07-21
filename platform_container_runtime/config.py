4ae33fbfeeaaf5720ea5e75593066615d7a1b54f
Ivan Zubenko <vanya.zubenko@gmail.com>
2021-07-21 13:22:39 +0300

    refactor

--- platform_container_runtime/config.py
+++ platform_container_runtime/config.py
@@ -1,6 +1,5 @@
-import os
 from dataclasses import dataclass
-from typing import Dict, Optional
+from typing import Optional
 
 from yarl import URL
 
@@ -32,45 +31,3 @@ class Config:
     cri_address: str
     zipkin: Optional[ZipkinConfig] = None
     sentry: Optional[SentryConfig] = None
-
-
-class EnvironConfigFactory:
-    def __init__(self, environ: Optional[Dict[str, str]] = None) -> None:
-        self._environ = environ or os.environ
-
-    def create(self) -> Config:
-        return Config(
-            server=self._create_server(),
-            cri_address=self._environ["NP_CRI_ADDRESS"],
-            zipkin=self.create_zipkin(),
-            sentry=self.create_sentry(),
-        )
-
-    def _create_server(self) -> ServerConfig:
-        host = self._environ.get("NP_HOST", ServerConfig.host)
-        port = int(self._environ.get("NP_PORT", ServerConfig.port))
-        return ServerConfig(host=host, port=port)
-
-    def create_zipkin(self) -> Optional[ZipkinConfig]:
-        if "NP_ZIPKIN_URL" not in self._environ:
-            return None
-
-        url = URL(self._environ["NP_ZIPKIN_URL"])
-        app_name = self._environ.get("NP_ZIPKIN_APP_NAME", ZipkinConfig.app_name)
-        sample_rate = float(
-            self._environ.get("NP_ZIPKIN_SAMPLE_RATE", ZipkinConfig.sample_rate)
-        )
-        return ZipkinConfig(url=url, app_name=app_name, sample_rate=sample_rate)
-
-    def create_sentry(self) -> Optional[SentryConfig]:
-        if "NP_SENTRY_DSN" not in self._environ:
-            return None
-
-        return SentryConfig(
-            dsn=URL(self._environ["NP_SENTRY_DSN"]),
-            cluster_name=self._environ["NP_SENTRY_CLUSTER_NAME"],
-            app_name=self._environ.get("NP_SENTRY_APP_NAME", SentryConfig.app_name),
-            sample_rate=float(
-                self._environ.get("NP_SENTRY_SAMPLE_RATE", SentryConfig.sample_rate)
-            ),
-        )
